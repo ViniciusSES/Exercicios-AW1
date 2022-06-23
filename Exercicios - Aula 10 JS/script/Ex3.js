2fead5bad03b8d7589addcb84717ce9a36f8267d
vinicius-vses <viniciussouzaesp@gmail.com>
2022-06-22 21:36:57 -0300

    .

--- Exercicios - Aula 10 JS/script/Ex3.js
+++ Exercicios - Aula 10 JS/script/Ex3.js
@@ -9,9 +9,4 @@ let carro = {
 document.write("Preço com 5% off pagando a vista: " + "R$" + ((carro.preco * 95) / 100));
 document.write(" | 50% de entrada: " + "R$" +  ((carro.preco / 2)));
 document.write(" | O resto dividido em 12 parcelas " + "R$" + ((carro.preco / 2) / 12));
-//}
-
-/*function aVista(){
-    carro.preco = preco - (5/100)
-}*/
-    
+//}    

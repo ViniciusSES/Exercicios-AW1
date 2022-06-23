30b8e94f4b311712fea15d3a198b3b650495ab4c
vinicius-vses <viniciussouzaesp@gmail.com>
2022-06-23 14:56:49 -0300

    .

--- Exercicios - Aula 10 JS/script/Ex3.js
+++ Exercicios - Aula 10 JS/script/Ex3.js
@@ -1,12 +1,14 @@
 let carro = {
-    marca: "Honda",
-    modelo: "City",
-    ano: "2020",
-    preco: "85000",
+    marca: "VW",
+    modelo: "Fusca",
+    ano: "1982",
+    preco: "20000",
 };
 
 //function calcular(){
-document.write("Preço com 5% off pagando a vista: " + "R$" + ((carro.preco * 95) / 100));
-document.write(" | 50% de entrada: " + "R$" +  ((carro.preco / 2)));
-document.write(" | O resto dividido em 12 parcelas " + "R$" + ((carro.preco / 2) / 12));
+    document.write("Marca: " + (carro.marca) + "<br> Modelo: " + (carro.modelo) +
+    "<br> Ano: " + (carro.ano) + "<br> Preço: " + (carro.preco) + "<br>")
+    document.write("<br> Preço com 5% off pagando a vista: " + "R$" + ((carro.preco * 95) / 100));
+    document.write("<br> 50% de entrada: " + "R$" +  ((carro.preco / 2)));
+    document.write("<br> O resto dividido em 12 parcelas " + "R$" + ((carro.preco / 2) / 12));
 //}    

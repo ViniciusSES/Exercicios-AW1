90d2e048c4c9a36194dc915499fc5040b40b292b
vinicius-vses <viniciussouzaesp@gmail.com>
2022-06-22 21:27:22 -0300

    .

--- Exercicios - Aula 10 JS/script/Ex3.js
+++ Exercicios - Aula 10 JS/script/Ex3.js
@@ -1,14 +1,15 @@
 let carro = {
-    marca: Honda,
-    modelo: City,
-    ano: 2020,
-    preco: 85000,
-    aVista: function(){
-        console.log("Preço com 5% off: " + carro.preco - (5/100));
-    }
+    marca: "Honda",
+    modelo: "City",
+    ano: "2020",
+    preco: "85000",
 };
 
-console.log(carro.ano);
+//function calcular(){
+document.write("Preço com 5% off pagando a vista: " + "R$" + ((carro.preco * 95) / 100));
+document.write("; 50% de entrada: " + "R$" +  ((carro.preco / 2)));
+document.write("; O resto dividido em 12 parcelas " + "R$" + ((carro.preco / 2) / 12));
+//}
 
 /*function aVista(){
     carro.preco = preco - (5/100)
